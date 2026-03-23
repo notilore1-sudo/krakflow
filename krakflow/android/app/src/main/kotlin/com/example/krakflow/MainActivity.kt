bf53d592309425a833a308f6fd9af1c3303a2444
notilore1-sudo <-email@.com>
2026-03-23 22:13:23 +0100

    lab_03 krakflow home screen

---
+++ krakflow/android/app/src/main/kotlin/com/example/krakflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.krakflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ krakflow/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KrakFlow"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text("KrakFlow"),
+              Text("Organizacja studiów"),
+              Text("Dzisiejsze zadania"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
